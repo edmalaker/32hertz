4b9ecda82c4ad599d86366a920c6f040b5b445cd
Ed Malaker <edmalaker@gmail.com>
2017-06-23 22:52:58 -0400

    Add files via upload

---
+++ 32hertzHome.php
@@ -0,0 +1,54 @@
+<div class="container-fluid homeback hgt2"><!-- begin container -->
+            
+    <!-- begin content -->
+
+    <!-- row 1 TOP NAV -->
+    <div class="row gfont cntr brown-back blog-border">
+        <div class="col-md-3">
+        <img src="http://32hertz.com/wp-content/uploads/2017/05/logo2-resized-min.jpg" alt="Logo" width="50%" height="50%" class="size-large wp-image-1452" />
+         </div>
+
+    <!-- col 2 NAV -->
+         <div class="col-md-3 col-md-offset-6">
+
+             <ul class="list-inline fontsize4 navcolor">
+                 <li class="tenpx-margin"><a title="About" href="http://32hertz.com/about/">About</a></li>
+                 <li class="tenpx-margin"><a title="Contact" href="http://32hertz.com/contact/">Contact</a></li>
+             </ul>
+
+          </div>
+
+    </div>
+    <div class="clearfix"></div>
+     
+    <!-- end row 1 TOP NAV -->
+
+
+    <!-- row 3 LINKS -->
+    <div class="row gfont cntr fontsize4 hgt3">
+
+        <div class="col-md-3 blog-border top-text-margin brown-back">
+
+            <div class="list-group top-text-margin">
+            <a class="list-group-item" title="Malaker Web Design" href="http://malaker.32hertz.com" target="_blank">Web Design</a>
+<a class="list-group-item" title="Malaker Web Design Portfolio" href="http://32hertz.com/portfolio" target="_blank">Web Design Portfolio</a>
+            <a class="list-group-item" title="Get A Record Deal: 100 Record Labels Accepting Demos" href="http://32hertz.com/get-a-record-deal-100-record-labels-accepting-demos/" target="_blank">Books</a>
+            <a class="list-group-item" title="Malaker Blog" href="http://32hertz.com/blog/">Blog</a>
+            <a class="list-group-item" title="Get My Song Reviewed" href="http://getmysongreviewed.com" target="_blank">getmysongreviewed.com</a>
+        </div>
+    </div>
+
+</div><!-- end row 3 -->
+
+
+<!-- Structured Data -->
+<script type="application/ld+json">
+    {
+    "@context": "http://schema.org",
+    "@type": "WebPage",
+    "name": "32Hertz",
+    "author": "Ed Malaker",
+    "primaryImageOfPage": "http://32hertz.com/wp-content/uploads/2017/05/logo2-resized-min.jpg",
+    "description": "Web Design, Game and Application Development, Original Music, Audio and Video Production."
+    }
+</script>
